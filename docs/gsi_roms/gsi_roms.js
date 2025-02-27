7a573d5c674985581db677cc6378a707e8d1b72e
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-02-27 03:21:22 +0100

    Use correct redirect for PixelOS Gsi

--- docs/gsi_roms/gsi_roms.js
+++ docs/gsi_roms/gsi_roms.js
@@ -2,7 +2,7 @@ export const a14 = {
     platinum: [],
     gold: [{
         name: "PixelOS 14 August release (MisterZtr)",
-        link: "a14/misterztr_pixelos_14_august"
+        link: "a14/misterztr_pixelos_14"
     }],
     silver: [],
     bronze: [],

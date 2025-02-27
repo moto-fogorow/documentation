832ac83baa45b62e8a6512c15a5eaa157cd7f6ae
hkamori <hk1337@proton.me>
2025-02-27 03:09:19 +0100

    Create gsi_roms.js

---
+++ docs/gsi_roms/gsi_roms.js
@@ -0,0 +1,10 @@
+export const a14 = {
+    platinum: [],
+    gold: [{
+        name: "PixelOS 14 August release (MisterZtr)",
+        link: "a14/misterztr_pixelos_14_august"
+    }],
+    silver: [],
+    bronze: [],
+    broken: []
+};
